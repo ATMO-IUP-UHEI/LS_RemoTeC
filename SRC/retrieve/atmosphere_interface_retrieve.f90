07a5ed088a512e822709d771ebaea427add1d9a7
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2023-10-05 17:18:31 +0200

    if trace gas info is not available in ATM file, set it to zero for all levels

--- SRC/retrieve/atmosphere_interface_retrieve.f90
+++ SRC/retrieve/atmosphere_interface_retrieve.f90
@@ -104,15 +104,28 @@ contains
       call netcdf_get_vector_var(ncid, "geometric_altitude", atm_scenario%z, start3d)
 
       ! get trace gases only if they are in the netcdf file
-      call netcdf_get_vector_var(ncid, "h2o", atm_scenario%h2o, start3d)
+      if (nf90_inq_varid(ncid, "h2o", i) /= nf90_enotvar) then
+         call netcdf_get_vector_var(ncid, "h2o", atm_scenario%h2o, start3d)
+      else
+         atm_scenario%h2o = 0
+      end if
+
       if (nf90_inq_varid(ncid, "co2", i) /= nf90_enotvar) then
          call netcdf_get_vector_var(ncid, "co2", atm_scenario%co2, start3d)
+      else
+         atm_scenario%co2 = 0
       end if
+
       if (nf90_inq_varid(ncid, "ch4", i) /= nf90_enotvar) then
          call netcdf_get_vector_var(ncid, "ch4", atm_scenario%ch4, start3d)
+      else
+         atm_scenario%ch4 = 0
       end if
+
       if (nf90_inq_varid(ncid, "co", i) /= nf90_enotvar) then
          call netcdf_get_vector_var(ncid, "co", atm_scenario%co, start3d)
+      else
+         atm_scenario%co = 0
       end if
 
       meta%surface_elevation = atm_scenario%z(nlevel)
