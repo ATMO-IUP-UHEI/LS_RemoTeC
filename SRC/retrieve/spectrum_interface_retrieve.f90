fca5dbc4a915eceebc54b058c088074316ff8e60
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2024-05-02 11:08:06 +0200

    corrected incorrect git merge

--- SRC/retrieve/spectrum_interface_retrieve.f90
+++ SRC/retrieve/spectrum_interface_retrieve.f90
@@ -134,13 +134,8 @@ contains
             allocate(wavelength(nwave))
             call netcdf_get_vector_var(grpid(band), "wavelength", wavelength, start=(/1/))
 
-<<<<<<< HEAD
-            ! Check if current band surrounds current fit window. If not, go to the next band
-            if (wavelength(1) > win_ini(win)%wave_start .or. win_ini(win)%wave_stop > wavelength(nwave)) then
-=======
             ! Check if current band surrounds current fit window. If not, go to the next band.
             if (wavelength(1) > win_ini(win)%wave_start .or. wavelength(nwave) < win_ini(win)%wave_stop) then
->>>>>>> when checking if current data band surrounds fit window, checking for fwhm multiple offset is not necessary
                if (band == nband) then
                   print*, "ERROR IN READ_L1B: No bands surround fit window."
                end if
@@ -364,14 +359,15 @@ contains
 
       end if
 
-!!$     do n = 1, nband
-!!$        write(ch,'(i2.2)') n
-!!$        open(50, file='isrf_'//ch//'.dat')
-!!$        do i = 1, response_out(1,n)%nils
-!!$           write(50,*) response_out(1,n)%ils_dwave(1,i), response_out(1,n)%resp_store(1,i)
-!!$        enddo
-!!$        close(50)
-!!$     enddo
+      ! TODO LS: give this a debug flag
+      do n = 1, nband
+         write(ch, '(i2.2)') n
+         open(50, file='used_isrf_'//ch//'.dat')
+         do i = 1, response_out(1, n)%nils
+            write(50, *) response_out(1, n)%ils_dwave(1, i), response_out(1, n)%resp_store(1, i)
+         enddo
+         close(50)
+      enddo
 
       ierr = 0
       return
